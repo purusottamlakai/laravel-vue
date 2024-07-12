d1939bf5a6774a5913b2539af76108d9a765035c
Purusottam Lakai <purusottam.lakai@nitv.com.np>
2024-07-12 16:24:26 +0545

    login system added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,11 @@ class HomeController extends Controller
     public function index() : \Illuminate\View\View {
         return view('app');
     }
+
+    public function getHomeData() {
+        return response()->json([
+            'user_count' => \App\Models\User::count(),
+            'product_count' => \App\Models\Product::count()
+        ]);
+    }
 }
